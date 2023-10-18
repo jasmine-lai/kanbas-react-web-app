427517741c558515711399d6235225f50354374e
Jasmine Lai <jasmine.lai.03@gmail.com>
2023-10-18 01:48:04 -0400

    add boilerplate for courses

--- src/Kanbas/index.js
+++ src/Kanbas/index.js
@@ -1,6 +1,7 @@
 import KanbasNavigation from "./KanbasNavigation";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
+import Courses from "./Courses";
 
 function Kanbas() {
     return (
@@ -8,11 +9,11 @@ function Kanbas() {
             <KanbasNavigation/>
             <div>
                 <Routes>
-                    <Route path="/"             element={<Navigate to="Dashboard"/>}/>
-                    <Route path="Account"       element={<h1>Account</h1>}/>
-                    <Route path="Dashboard"     element={<Dashboard/>}/>
-                    <Route path="Courses/*"     element={<h1>Courses</h1>}/>
-                    <Route path="Calendar/*"    element={<h1>Calendar</h1>}/>
+                    <Route path="/"                       element={<Navigate to="Dashboard"/>}/>
+                    <Route path="Account"                 element={<h1>Account</h1>}/>
+                    <Route path="Dashboard"               element={<Dashboard/>}/>
+                    <Route path="Courses/:courseId/*"     element={<Courses/>}/>
+                    <Route path="Calendar/*"              element={<h1>Calendar</h1>}/>
                 </Routes>
             </div>
         </div>
