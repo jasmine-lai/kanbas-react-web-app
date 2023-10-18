bdcb72e41aa6bc7bde2b6ec526bf2cc50c95aa8a
Jasmine Lai <jasmine.lai.03@gmail.com>
2023-10-18 14:23:22 -0400

    mostly finished courses

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -1,36 +1,47 @@
 import db from "../../Kanbas/Database";
-import { Navigate, Route, Routes, useParams } from "react-router-dom";
+import { Link, Navigate, Route, Routes, useParams } from "react-router-dom";
+import { HiBars3 } from "react-icons/hi2";
 import CourseNavigation from "./CourseNavigation";
+import Modules from "./Modules";
+import "./index.css"
 
 function Courses() {
   const { courseId } = useParams();
   const course = db.courses.find((course) => course._id === courseId);
   return (
-    <div>
-      <h1>Course {course.name}</h1>
-      <CourseNavigation/>
-      <div>
-        <div
-          className="overflow-y-scroll position-fixed bottom-0 end-0"
-          style={{
-            left: "320px",
-            top: "50px",
-          }}
+    <div className="courses">
+      <div className="row mt-3 ms-0">
+        <HiBars3 className="text icon col-1 ps-0" size="35"/>
+        <nav aria-label="breadcrumb" className="mb-0 col-9">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <Link key={course._id} to={`/Kanbas/Courses/${course._id}`} className="breadcrumb-link">
+                {course.number}.{course._id}
+              </Link>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">Home</li>
+          </ol>
+        </nav>
+      </div>
+      <hr className="mt-2 ms-4"/>
+      <div className="row mt-4 ms-1">
+      <CourseNavigation className="col-3"/>
+      <div
+          className="overflow-y-scroll col-9"
         >
           <Routes>
-            <Route path="/" element={<Navigate to="Home" />} />
-            <Route path="Home" element={<h1>Home</h1>} />
-            <Route path="Modules" element={<h1>Modules</h1>} />
-            <Route path="Assignments" element={<h1>Assignments</h1>} />
+            <Route path="/" element={<Navigate to="Home" />}/>
+            <Route path="Home" element={<Modules/>}/>
+            <Route path="Modules" element={<Modules/>}/>
+            <Route path="Assignments" element={<h1>Assignments</h1>}/>
             <Route
               path="Assignments/:assignmentId"
               element={<h1>Assignment Editor</h1>}
             />
-            <Route path="Grades" element={<h1>Grades</h1>} />
+            <Route path="Grades" element={<h1>Grades</h1>}/>
           </Routes>
-        </div>
       </div>
-
+      </div>
     </div>
   );
 }
