d646fef7ce70f57b871e0d4458d76938cc7b3579
Jasmine Lai <jasmine.lai.03@gmail.com>
2023-11-01 13:58:10 -0400

    Finished a4 dashboard

--- src/Kanbas/Dashboard/index.js
+++ src/Kanbas/Dashboard/index.js
@@ -1,3 +1,4 @@
+import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import { HiOutlineEllipsisVertical } from "react-icons/hi2";
 import { TfiWrite } from "react-icons/tfi";
@@ -5,8 +6,7 @@ import db from "../Database";
 import "./index.css"
 import "../index.css"
 
-function Dashboard() {
-  const courses = db.courses;
+function Dashboard({ courses, course, setCourse, addNewCourse, deleteCourse, updateCourse }) {
   const colorMap = {
     19753: "color-turquoise", 
     16840: "color-indigo", 
@@ -18,6 +18,19 @@ function Dashboard() {
       <hr className="mt-2 ms-4"/>
       <h2 className="ms-5 font-slim">Published Courses (3)</h2>
       <hr className="ms-5"/>
+      <div className="edit-dashboard ms-5">
+        <h5>Edit Dashboard</h5>
+        <input value={course.name} className="form-control" placeholder="Course Name"
+              onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
+        <input value={course.number} className="form-control" placeholder="Course Number"
+              onChange={(e) => setCourse({ ...course, number: e.target.value }) } />
+        <input value={course.startDate} className="form-control" type="date"
+              onChange={(e) => setCourse({ ...course, startDate: e.target.value }) }/>
+        <input value={course.endDate} className="form-control" type="date"
+              onChange={(e) => setCourse({ ...course, endDate: e.target.value }) } />
+        <button onClick={addNewCourse} className="btn btn-success mt-2">Add</button>
+        <button onClick={updateCourse} className="btn btn-primary mt-2 ms-2">Update</button>
+      </div>
       {/* TODO: fix card alignment */}
       <div className="list-group ms-5">
         {courses.map((course) => (
@@ -32,7 +45,21 @@ function Dashboard() {
                     <p class="secondary-text mt-0">
                       {course.number}.{course._id}
                     </p>
-                    <TfiWrite className="text" size="20"/>
+                    <button onClick={(event) => {
+                        event.preventDefault();
+                        setCourse(course);
+                      }}
+                        className="btn btn-light">
+                      <TfiWrite className="text mb-1 me-2" size="20"/>
+                      Edit
+                    </button>
+                    <button onClick={(event) => {
+                        event.preventDefault();
+                        deleteCourse(course._id);
+                      }} 
+                        className="btn btn-danger float-end">
+                      Delete
+                    </button>
                 </div>
               </div>
             </div>
